aaf9a7dd09c00573008f9fc9d0e9bf9b6dfb5434
theOguz16 <98805835+theOguz16@users.noreply.github.com>
2026-09-09 21:14:10 +0300

    fix: keep storefront display name optional until configured

--- packages/db/drizzle/0015_branded_storefront_context.sql
+++ packages/db/drizzle/0015_branded_storefront_context.sql
@@ -10,6 +10,4 @@ ALTER TABLE "merchants" ADD COLUMN "is_public" boolean DEFAULT false NOT NULL;
 --> statement-breakpoint
 UPDATE "merchants" SET "display_name" = "name" WHERE "display_name" IS NULL;
 --> statement-breakpoint
-ALTER TABLE "merchants" ALTER COLUMN "display_name" SET NOT NULL;
---> statement-breakpoint
 ALTER TABLE "merchants" ADD CONSTRAINT "merchant_primary_color" CHECK ("merchants"."primary_color" ~ '^#[0-9A-Fa-f]{6}$');
